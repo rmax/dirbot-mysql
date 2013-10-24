a5b80fa1d4f1e174f2949ece14efb14c47e54c53
Rolando Espinoza La fuente <darkrho@gmail.com>
2013-10-24 18:22:53 -0400

    Added mysql items store.

--- dirbot/settings.py
+++ dirbot/settings.py
@@ -4,4 +4,13 @@ SPIDER_MODULES = ['dirbot.spiders']
 NEWSPIDER_MODULE = 'dirbot.spiders'
 DEFAULT_ITEM_CLASS = 'dirbot.items.Website'
 
-ITEM_PIPELINES = ['dirbot.pipelines.FilterWordsPipeline']
+ITEM_PIPELINES = [
+    'dirbot.pipelines.RequiredFieldsPipeline',
+    'dirbot.pipelines.FilterWordsPipeline',
+    'dirbot.pipelines.MySQLStorePipeline',
+]
+
+MYSQL_HOST = 'localhost'
+MYSQL_DBNAME = 'dirbot'
+MYSQL_USER = 'dirbot'
+MYSQL_PASSWD = ''
